70ca3d789687d3d5be752d81e567143198044e08
João Pedro Bacarin <112831294+JPBaccarin@users.noreply.github.com>
2023-08-12 16:26:47 -0300

    melhoria na navbar, adição do Framer Motion

--- client/learnspace/src/components/modal/TutorialModal.tsx
+++ client/learnspace/src/components/modal/TutorialModal.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface Step {
   title: string;
@@ -28,39 +29,62 @@ const TutorialModal: React.FC<TutorialModalProps> = ({ isOpen, steps, onClose })
   };
 
   return (
-    <div className={`fixed inset-0 flex items-center justify-center z-50 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-      {/* Blur de fundo */}
-      {isOpen && <div className="fixed inset-0 bg-black/50  " onClick={onClose}></div>}
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="fixed inset-0 flex items-center justify-center z-50"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          {/* Blur de fundo */}
+          <div
+            className="fixed inset-0 bg-black/50"
+            onClick={onClose}
+          ></div>
 
-      <div className="bg-white dark:bg-gray-800  p-4 rounded-md shadow-sm shadow-blue-500/20 border border-white/10 transition-opacity duration-300 relative">
-        <img src={steps[currentStep].imageSrc} alt={steps[currentStep].title} className="w-40 mx-auto mb-4" />
-        <h2 className="text-xl font-bold mb-2">{steps[currentStep].title}</h2>
-        <p className="mb-4">{steps[currentStep].description}</p>
-        <div className="flex justify-between">
-          <button
-            className={`px-4 py-2 bg-blue-500 hover:bg-blue-600 transition-all text-white rounded ${currentStep === 0 ? 'invisible' : ''}`}
-            onClick={handlePreviousStep}
+          <motion.div
+            className="bg-white dark:bg-gray-800 p-4 rounded-md shadow-sm shadow-blue-500/20 border border-white/10 transition-opacity duration-300 relative"
+            initial={{ scale: 0.5, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.5, opacity: 0 }}
+            transition={{ duration: 0.3 }}
           >
-            Anterior
-          </button>
-          {currentStep < steps.length - 1 ? (
-            <button
-              className="px-4 py-2 bg-blue-500 hover:bg-blue-600 transition-all text-white rounded"
-              onClick={handleNextStep}
-            >
-              Próximo
-            </button>
-          ) : (
-            <button
-              className="px-4 py-2 bg-blue-500 hover:bg-blue-600 transition-all text-white rounded"
-              onClick={onClose}
-            >
-              Sair
-            </button>
-          )}
-        </div>
-      </div>
-    </div>
+            <img
+              src={steps[currentStep].imageSrc}
+              alt={steps[currentStep].title}
+              className="w-40 mx-auto mb-4"
+            />
+            <h2 className="text-xl font-bold mb-2">{steps[currentStep].title}</h2>
+            <p className="mb-4">{steps[currentStep].description}</p>
+            <div className="flex justify-between">
+              <button
+                className={`px-4 py-2 bg-blue-500 hover:bg-blue-600 transition-all text-white rounded ${currentStep === 0 ? 'invisible' : ''}`}
+                onClick={handlePreviousStep}
+              >
+                Anterior
+              </button>
+              {currentStep < steps.length - 1 ? (
+                <button
+                  className="px-4 py-2 bg-blue-500 hover:bg-blue-600 transition-all text-white rounded"
+                  onClick={handleNextStep}
+                >
+                  Próximo
+                </button>
+              ) : (
+                <button
+                  className="px-4 py-2 bg-blue-500 hover:bg-blue-600 transition-all text-white rounded"
+                  onClick={onClose}
+                >
+                  Sair
+                </button>
+              )}
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
