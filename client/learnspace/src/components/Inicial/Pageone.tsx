afe8033f0f1ba44d78a3deab34cea3835ddb9752
padreqvedo <felipecoelhofreiredonascimento@gmail.com>
2023-08-28 15:14:36 -0300

    bf

--- client/learnspace/src/components/Inicial/Pageone.tsx
+++ client/learnspace/src/components/Inicial/Pageone.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export default function Pageone() {
   return (
     <div className=" flex items-top justify-center bg-gray-800 text-6xl font-semibold text-white py-10 " >
