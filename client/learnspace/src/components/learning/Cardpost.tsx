3cc9a92fd8cb99f5b153e1deb96020fc10f37b1b
João Pedro Bacarin <112831294+JPBaccarin@users.noreply.github.com>
2023-09-26 21:32:50 -0300

    CMS quase pronto (falta o blog)

--- client/learnspace/src/components/learning/Cardpost.tsx
+++ client/learnspace/src/components/learning/Cardpost.tsx
@@ -1,43 +1,47 @@
-// src/Card.tsx
 import React from 'react';
+import { motion, spring } from 'framer-motion';
 
 interface CardpostProps {
     title: string;
-    description: string;
+    summary: string;
     linkUrl: string;
     imageUrl: string;
-    imageAlt: string;
+    category: string;
 }
 
-const Cardpost: React.FC<CardpostProps> = ({ title, description, linkUrl, imageUrl, imageAlt }) => {
+const Cardpost: React.FC<CardpostProps> = ({ title, summary, linkUrl, imageUrl, category }) => {
     return (
-        <div className="bg-gray-700 w-full  sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 justify-around shadow-md text-white rounded-lg relative">
-            <div
-                className="h-40 rounded-t-lg"
-                style={{
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    backgroundImage: `url(${imageUrl})`,
-                }}
-            ></div>
+        <motion.div
+        className="bg-gray-700 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 justify-around shadow-md text-white rounded-lg relative m-2"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5, type:"spring", stiffness:"200"  }}
+        whileHover={{ scale: 1.02 }}
+    >
+        <div
+            className="h-40 rounded-t-lg"
+            style={{
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundImage: `url(${imageUrl})`,
+            }}
+        ></div>
 
-            <div className="absolute top-0 left-0 m-2">
-                <div className="bg-yellow-500 rounded-md text-sm p-1">teste</div>
-            </div>
-
-
-            <div className="bg-black h-44 p-4 text-sm">
-                <h2 className="text-lg font-bold mb-2">{title}</h2>
-                <p className="">{description}</p>
-                <a
-                    href={linkUrl}
-                    className="text-blue-600 mt-2 inline-block hover:text-blue-500 transition-all hover:underline"
-                >
-                    Leia mais
-                </a>
-            </div>
-
+        <div className="absolute top-0 left-0 m-2">
+            <div className="bg-yellow-500 rounded-md text-sm p-1">{category}</div>
         </div>
+
+        <div className="bg-black h-44 p-4 text-sm rounded-b-md">
+            <h2 className="text-lg font-bold mb-2">{title}</h2>
+            <p className="">{summary}</p>
+            <a
+                href={linkUrl}
+                className="text-blue-600 mt-2 inline-block hover:text-blue-500 transition-all hover:underline"
+            >
+                Leia mais
+            </a>
+        </div>
+    </motion.div>
     );
 };
 
