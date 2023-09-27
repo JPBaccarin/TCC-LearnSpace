ad0db8f33d82aa9ebdba1ca64a6293c2b835801d
João Pedro Bacarin <112831294+JPBaccarin@users.noreply.github.com>
2023-09-27 18:26:58 -0300

    cms funcionando e quiz estatico novamente

--- client/learnspace/src/app/learning/[slug]/page.tsx
+++ client/learnspace/src/app/learning/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import config from '../config';
 import fetchBlogs from '../fetch-blogs';
 import ReactMarkdown from 'react-markdown'
-
+import './index.css'
 
 interface BlogData {
     id: number;
@@ -39,7 +39,29 @@ interface BlogData {
                             size: number;
                             url: string;
                         };
+                        large: {
+                            name: string;
+                            hash: string;
+                            ext: string;
+                            mime: string;
+                            path: string | null;
+                            width: number;
+                            height: number;
+                            size: number;
+                            url: string;
+                        };
                         small: {
+                            name: string;
+                            hash: string;
+                            ext: string;
+                            mime: string;
+                            path: string | null;
+                            width: number;
+                            height: number;
+                            size: number;
+                            url: string;
+                        };
+                        medium: {
                             name: string;
                             hash: string;
                             ext: string;
@@ -65,8 +87,81 @@ interface BlogData {
                 };
             };
         };
+        FeaturedImage: {
+            data: {
+                id: number;
+                attributes: {
+                    name: string;
+                    alternativeText: string | null;
+                    caption: string | null;
+                    width: number;
+                    height: number;
+                    formats: {
+                        thumbnail: {
+                            name: string;
+                            hash: string;
+                            ext: string;
+                            mime: string;
+                            path: string | null;
+                            width: number;
+                            height: number;
+                            size: number;
+                            url: string;
+                        };
+                        large: {
+                            name: string;
+                            hash: string;
+                            ext: string;
+                            mime: string;
+                            path: string | null;
+                            width: number;
+                            height: number;
+                            size: number;
+                            url: string;
+                        };
+                        small: {
+                            name: string;
+                            hash: string;
+                            ext: string;
+                            mime: string;
+                            path: string | null;
+                            width: number;
+                            height: number;
+                            size: number;
+                            url: string;
+                        };
+                        medium: {
+                            name: string;
+                            hash: string;
+                            ext: string;
+                            mime: string;
+                            path: string | null;
+                            width: number;
+                            height: number;
+                            size: number;
+                            url: string;
+                        };
+                        // Adicione outros tamanhos de imagem, se necessário
+                    };
+                    hash: string;
+                    ext: string;
+                    mime: string;
+                    size: number;
+                    url: string;
+                    previewUrl: string | null;
+                    provider: string;
+                    provider_metadata: string | null;
+                    createdAt: string;
+                    updatedAt: string;
+                };
+            }[];
+        };
+        localizations: {
+            data: any[]; // Você pode substituir 'any' pelo tipo correto, se necessário
+        };
     };
-};
+}
+
 
 const BlogPage = async (props: any) => {
 
@@ -76,23 +171,21 @@ const BlogPage = async (props: any) => {
     if (blogs.data.length === 0) return null;
     const blog = blogs.data[0];
 
-
     return (
         <div className='bg-gray-800 bgsvg w-full h-screen dark:text-white flex justify-center items-center'>
             <div className='bg-gray-700 flex flex-col w-2/3 rounded-md p-5'>
-                <h2 className='text-center text-2xl font-bold'>{blog.attributes.Title}</h2>
-                <img src={blog.atributes} alt="" />
+                <h2 className='text-center text-4xl font-bold'>{blog.attributes.Title}</h2>
                 <div className='px-4 py-2'>
                     <ReactMarkdown>{blog.attributes.Content}</ReactMarkdown>
                 </div>
-            </div>
+                           </div>
         </div>
     )
 }
 
 export const generateStaticParams = async () => {
 
-    const blogs = await fetchBlogs();
+    const blogs = await fetchBlogs(null);
 
     return blogs.data.map((blog: any) => ({
         slug: blog.attributes.slug,
